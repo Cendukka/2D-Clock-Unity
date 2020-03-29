cbb6a06478a47892c88aa66a18b35b3a1f92d738
Samuli Lehtonen <samulilehtonen33@gmail.com>
2020-03-29 19:23:07 +0300

    Added better pointers for analog version

--- 2D-Clock/Assets/Scripts/Clock.cs
+++ 2D-Clock/Assets/Scripts/Clock.cs
@@ -4,7 +4,16 @@
  * Creation date: 29.03.2020
  * Short script description: This script gets system's current clock time and shows it as a digital type and analog type clock.
  * 
- ** How to use the script:
+ ********How the script works***********
+ * 
+ *** The script takes the current system time every frame, and splits it in to 3 different variable: Seconds, minutes and hours.
+ *** Then it displays the time in the digital clock.
+ *** Then the function rotateAnalogClock() in FixedUpdate rotates the pointers every fixed frame-rate frame according the current time and the correct angle.
+ * 
+ * 
+ * 
+ ********How to use the script***********
+ * 
  *** 1. First you need to create Clock frames for the analog and digital clock.
  *** 2. Then you need to have 3 gameobjects that represents seconds, minutes and hour pointers named as: AnaSeconds, AnaMinutes and AnaHours
  *** 3. Then you need to have 3 text objects and attach them correctly for the script in Unity inspector: Digi Second Text, Digi Minute Text and Digi Hour Text;
